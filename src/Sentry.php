daa01685e6f9393abd2c803bab5771eb1f10f5c9
Rei Junior <34229270+masterei@users.noreply.github.com>
2025-06-25 15:48:11 +0800

    Update Sentry.php

--- src/Sentry.php
+++ src/Sentry.php
@@ -31,7 +31,7 @@ class Sentry extends Model
         static::addGlobalScope('published', function (Builder $builder) {
             $builder->select([
                 self::baseTable() . '.id',
-                 self::baseTable() . '.permission_id',
+                self::baseTable() . '.permission_id',
                 self::permissionTable() . '.name as uri',
                 self::baseTable() . '.method',
                 self::permissionTable() . '.guard_name',
